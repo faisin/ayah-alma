12ba16dbd6f998e762330cd5e7af96d946923c86
faisin <83275517+faisin@users.noreply.github.com>
2026-09-02 20:49:33 +0800

    Create member-ssh.sh

---
+++ ssh/member-ssh.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+clear
+
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "      LIST SSH ACCOUNTS"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo ""
+
+# Membaca daftar user dari file passwd dengan shell /bin/bash atau /bin/sh
+echo "No  | Username         | Expired Date"
+echo "-------------------------------------"
+
+no=1
+while IFS: read -r user x uid gid comment home shell; do
+    if [[ "$shell" == *"/bash"* || "$shell" == *"/sh"* ]]; then
+        # Mengabaikan user sistem standar
+        if [[ "$user" != "root" && "$user" != "daemon" && "$user" != "bin" && "$user" != "sys" && "$user" != "sync" && "$user" != "games" && "$user" != "man" && "$user" != "mail" && "$user" != "news" && "$user" != "uucp" && "$user" != "proxy" && "$user" != "www-data" && "$user" != "backup" && "$user" != "list" && "$user" != "irc" && "$user" != "gnats" && "$user" != "nobody" && "$user" != "systemd-network" && "$user" != "systemd-resolve" && "$user" != "messagebus" && "$user" != "_apt" && "$user" != "uuidd" && "$user" != "dnsmasq" && "$user" != "stunnel4" ]]; then
+            
+            # Mendapatkan tanggal expired akun menggunakan chage
+            exp_date=$(chage -l "$user" | grep "Account expires" | cut -d: -f2 | xargs)
+            if [[ "$exp_date" == "never" || -z "$exp_date" ]]; then
+                exp_date="UNLIMITED"
+            fi
+            
+            printf "%-3s | %-16s | %s\n" "$no" "$user" "$exp_date"
+            no=$((no + 1))
+        fi
+    fi
+done < /etc/passwd
+
+echo ""
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+
